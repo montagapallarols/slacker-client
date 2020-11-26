f115c4d60a91638947d7bab1c8e98b057eb9729d
montipallarols <montaga.pallarols@gmail.com>
2020-11-26 15:06:02 +0100

    Add signup/login links to Home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
@@ -6,6 +8,13 @@ export default function HomePage() {
       <p>
         Welcome to Slacker! Here you can curate, review and share your favourite
         films, TV shows, books and podcasts or add them to your wish list.
+      </p>
+      <p>Sign up to create your profile and connect with your friends!</p>
+      <Link to="/signup">
+        <Button variant="dark">Sign up</Button>
+      </Link>
+      <p>
+        Already have an account? Log in <Link to="/login">here</Link>
       </p>
     </div>
   );
