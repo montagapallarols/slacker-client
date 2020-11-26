f08a43d163f28257880982f4060e4e6239d1f51d
montipallarols <montaga.pallarols@gmail.com>
2020-11-26 14:14:13 +0100

    Create Home page and display app description

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import HomePage from "../src/pages/Home/HomePage";
+
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/explore" component={Explore} />
-        <Route exact path="/explore" component={Reviews} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        <Route exact path="/explore" component={ExplorePage} />
+        <Route exact path="/explore" component={ReviewsPage} />
         <Route path="/profiles/:userId" component={ProfilePage} />
-        <Route path="/:userId" component={MyProfile} />
-      </Switch> */}
+        <Route path="/:userId" component={MyProfile} /> */}
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <p>
+        Welcome to Slacker! Here you can curate, review and share your favourite
+        films, TV shows, books and podcasts or add them to your wish list.
+      </p>
+    </div>
+  );
+}
