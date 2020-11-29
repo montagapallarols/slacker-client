2875c3afff9bac8c2d79a3e85da804a8c16a83c1
montipallarols <montaga.pallarols@gmail.com>
2020-11-29 20:32:39 +0100

    Change welcome message after login to include user's name

--- src/components/Navigation/LoggedIn.tsx
+++ src/components/Navigation/LoggedIn.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logOut } from "../../store/user/actions";
 import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
 import Nav from "react-bootstrap/Nav";
 import NavbarItem from "./NavbarItem";
-import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function LoggedIn() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
+  const history = useHistory();
+
+  // function handleOnClick() {
+  //   dispatch(logOut);
+  //   history.push("/");
+  // }
 
   return (
     <>
       <NavbarItem path={`/${user.id}`} linkText="My Profile" />
       <Nav.Item style={{ padding: ".5rem 1rem" }}>{user.firstName}</Nav.Item>
       <Button onClick={() => dispatch(logOut())}>Logout</Button>
+      {/* <Button onClick={handleOnClick}>Logout</Button> */}
     </>
   );
 }
