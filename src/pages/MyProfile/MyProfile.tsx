fb659c48060bd6ab1e82dd1b92e4218f599be81e
montipallarols <montaga.pallarols@gmail.com>
2020-11-29 16:58:32 +0100

    Display full name and round image in My Profile page

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./MyProfile.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserProfile } from "../../store/user/selectors";
 
 export default function MyProfile() {
+  const userProfile = useSelector(selectUserProfile);
+
   return (
     <div>
-      <h1>My Profile</h1>
+      <h1>{`${userProfile.firstName} ${userProfile.lastName}`}</h1>
+      <img src={userProfile.imageUrl} className="profile-image" />
     </div>
   );
 }
