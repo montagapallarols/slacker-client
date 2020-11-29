e4438f71a28a085da462a04baf2fc27580008613
montipallarols <montaga.pallarols@gmail.com>
2020-11-29 20:06:26 +0100

    Get user's favourites inside My Profile component

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -16,16 +16,23 @@ export default function MyProfile() {
   });
   console.log("Logged in user's lists", userProfileWithLists);
 
+  const userFavourites = allFavourites?.map((f: any) => {
+    if (f.list.profileId === userProfile.id) {
+      return f.item;
+    } else {
+      return null;
+    }
+  });
+  console.log("USER FAVOURITES:", userFavourites);
+
   return (
     <div>
       <h1>{`${userProfile.firstName} ${userProfile.lastName}`}</h1>
       <img src={userProfile.imageUrl} className="profile-image" />
       <p></p>
-      <p>
-        {userProfileWithLists?.lists.map((l: any) => {
-          return <h3>{l.type}</h3>;
-        })}
-      </p>
+      {userProfileWithLists?.lists.map((l: any) => {
+        return <h3>{l.type}</h3>;
+      })}
     </div>
   );
 }
