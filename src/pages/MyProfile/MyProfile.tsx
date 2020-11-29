e65ada07a034aa6892cb2cf51bf9d38fd34c6360
montipallarols <montaga.pallarols@gmail.com>
2020-11-29 17:20:22 +0100

    Select logged in user's lists

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./MyProfile.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectUserProfile } from "../../store/user/selectors";
+import { selectUserProfile, selectUser } from "../../store/user/selectors";
+import { selectAllProfiles } from "../../store/profiles/selectors";
 
 export default function MyProfile() {
+  const user = useSelector(selectUser);
   const userProfile = useSelector(selectUserProfile);
+  const allProfiles = useSelector(selectAllProfiles);
+
+  const userProfileLists = allProfiles?.find((p: any) => {
+    return p.userId === user.id;
+  });
+  console.log("Logged in user's lists", userProfileLists);
 
   return (
     <div>
       <h1>{`${userProfile.firstName} ${userProfile.lastName}`}</h1>
       <img src={userProfile.imageUrl} className="profile-image" />
+      <p></p>
+      <p>Hello</p>
     </div>
   );
 }
