7821262e7dc2cfadf98dea2ab96c4fdf3e5e2de8
montipallarols <montaga.pallarols@gmail.com>
2020-11-29 23:27:13 +0100

    Display Favourites list with favourite items in My Profile page

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -3,25 +3,30 @@ import "./MyProfile.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUserProfile, selectUser } from "../../store/user/selectors";
 import { selectAllProfiles } from "../../store/profiles/selectors";
-import { selectAllFavourites } from "../../store/listItems/selectors";
+import {
+  selectAllFavourites,
+  selectAllListItems,
+} from "../../store/listItems/selectors";
 
 export default function MyProfile() {
   const user = useSelector(selectUser);
   const userProfile = useSelector(selectUserProfile);
   const allProfiles = useSelector(selectAllProfiles);
   const allFavourites = useSelector(selectAllFavourites);
+  const allListItems = useSelector(selectAllListItems);
 
   const userProfileWithLists: any = allProfiles?.find((p: any) => {
     return p.userId === user.id;
   });
-  console.log("Logged in user's lists", userProfileWithLists);
+  console.log("User profile with lists", userProfileWithLists);
 
-  const userFavourites = allFavourites?.map((f: any) => {
-    if (f.list.profileId === userProfile.id) {
-      return f.item;
-    } else {
-      return null;
-    }
+  //   const profileListsNamesArray = userProfileWithLists?.lists.map((l: any) => {
+  //     return l.type;
+  //   });
+  //   console.log("Profile Lists Names:", profileListsNamesArray);
+
+  const userFavourites = allFavourites?.filter((f: any) => {
+    return f.list.profileId === userProfile.id;
   });
   console.log("USER FAVOURITES:", userFavourites);
 
@@ -30,9 +35,30 @@ export default function MyProfile() {
       <h1>{`${userProfile.firstName} ${userProfile.lastName}`}</h1>
       <img src={userProfile.imageUrl} className="profile-image" />
       <p></p>
-      {userProfileWithLists?.lists.map((l: any) => {
-        return <h3>{l.type}</h3>;
+
+      <h3>Favourites</h3>
+      {userFavourites?.map((f: any) => {
+        return (
+          <div>
+            <p>{f.item.name}</p>
+          </div>
+        );
       })}
+
+      {/* {userProfileWithLists?.lists.map((l: any) => {
+        return (
+          <div>
+            <h3 key={l.id}>{l.type}</h3>
+            {allListItems?.map((i: any) => {
+              return i.listId === l.id ? (
+                <div>
+                  <p>{i.item.name}</p>
+                </div>
+              ) : null;
+            })}
+          </div>
+        );
+      })} */}
     </div>
   );
 }
