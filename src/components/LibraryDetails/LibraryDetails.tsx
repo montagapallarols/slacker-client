87cba2bceb23b517bdb14e12a5c1e581ff6f3e7c
montipallarols <montaga.pallarols@gmail.com>
2020-11-30 11:39:22 +0100

    Display search bar in List Details component

--- src/components/LibraryDetails/LibraryDetails.tsx
+++ src/components/LibraryDetails/LibraryDetails.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
 
 export default function ListDetails() {
   interface ParamTypes {
@@ -10,6 +14,23 @@ export default function ListDetails() {
   return (
     <div>
       <h2>My {categoryName} Library</h2>
+      <Container>
+        <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>
+              Search for {categoryName.toLowerCase()} to add them to your
+              Library:
+            </Form.Label>
+            <Form.Control
+              type="search"
+              placeholder={`Search ${categoryName.toLowerCase()}...`}
+            />
+          </Form.Group>
+          <Button variant="dark" type="search">
+            Search
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
