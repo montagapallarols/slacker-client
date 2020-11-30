a2e80bef0713408b217748825a150447cedf01c5
montipallarols <montaga.pallarols@gmail.com>
2020-11-30 11:21:09 +0100

    Display dynamic title in Library details page

--- src/components/LibraryDetails/LibraryDetails.tsx
+++ src/components/LibraryDetails/LibraryDetails.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function ListDetails() {
-  //   const categoryName = useParams();
+  interface ParamTypes {
+    categoryName: string;
+  }
+  const { categoryName } = useParams<ParamTypes>();
+
   return (
     <div>
-      <h2>Library Details</h2>
+      <h2>My {categoryName} Library</h2>
     </div>
   );
 }
