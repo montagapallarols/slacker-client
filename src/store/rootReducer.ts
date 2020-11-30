42133694d807a390eb3251f12cdbc26b0e2def67
montipallarols <montaga.pallarols@gmail.com>
2020-11-30 14:18:53 +0100

    Add apiItems reducer to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,12 +3,14 @@ import appState from "./appState/reducer";
 import user from "./user/reducer";
 import profiles from "./profiles/reducer";
 import listItems from "./listItems/reducer";
+import apiItems from "./apiItems/reducer";
 
 export default combineReducers({
   appState,
   user,
   profiles,
   listItems,
+  apiItems,
 });
 
 export type RootState = ReturnType<typeof combineReducers>;
