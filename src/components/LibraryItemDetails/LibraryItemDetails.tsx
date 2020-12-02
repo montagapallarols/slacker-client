230eb1330e02bbc04f7c53a17d71517ffcbedc57
montipallarols <montaga.pallarols@gmail.com>
2020-12-02 17:58:21 +0100

    Implement add/remove from favpurites in Library details page

--- src/components/LibraryItemDetails/LibraryItemDetails.tsx
+++ src/components/LibraryItemDetails/LibraryItemDetails.tsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchApiItems, fetchApiItemById } from "../../store/apiItems/actions";
+import {
+  fetchApiItems,
+  fetchApiItemById,
+  fetchFavouriteApiItemById,
+} from "../../store/apiItems/actions";
 import {
   addItemToList,
   removeItemFromLibrary,
+  removeItemFromFavourites,
 } from "../../store/listItems/actions";
 import {
   selectApiItemsLoading,
   selectAllApiItems,
   selectApiItemDetails,
+  selectFavouriteApiItemDetails,
 } from "../../store/apiItems/selectors";
 import { selectUser } from "../../store/user/selectors";
 import {
@@ -23,6 +29,9 @@ export default function LibraryItemDetails() {
   const apiItemsLoading = useSelector(selectApiItemsLoading);
   const allApiItems = useSelector(selectAllApiItems);
   const apiItemDetails: any = useSelector(selectApiItemDetails);
+  const favouriteApiItemDetails: any = useSelector(
+    selectFavouriteApiItemDetails
+  );
   const user = useSelector(selectUser);
   const allCategories = useSelector(selectAllCategories);
   const allListItems = useSelector(selectAllListItems);
@@ -34,10 +43,22 @@ export default function LibraryItemDetails() {
       ? 2
       : null;
 
+  const favouriteCategoryId =
+    favouriteApiItemDetails.Type === "movie"
+      ? 1
+      : favouriteApiItemDetails.Type === "series"
+      ? 2
+      : null;
+
   const userLibraryList = user.profile.lists?.find((l: any) => {
     return l.type === "Library";
   });
   const userLibraryListId = userLibraryList.id;
+
+  const userFavouriteList = user.profile.lists?.find((l: any) => {
+    return l.type === "Favourites";
+  });
+  const userFavouriteListId = userFavouriteList.id;
 
   interface ParamTypes {
     itemId: string;
@@ -46,9 +67,14 @@ export default function LibraryItemDetails() {
   const { itemId } = useParams<ParamTypes>();
 
   useEffect(() => {
-    console.log("Item id", itemId);
+    // console.log("Item id", itemId);
     dispatch(fetchApiItemById(itemId));
   }, [dispatch, itemId]);
+
+  useEffect(() => {
+    console.log("Favourite Item id", itemId);
+    dispatch(fetchFavouriteApiItemById(itemId));
+  }, [dispatch, favouriteApiItemDetails]);
 
   function onClickAdd() {
     // console.log("api item details", apiItemDetails);
@@ -57,13 +83,30 @@ export default function LibraryItemDetails() {
     dispatch(addItemToList(apiItemDetails, categoryId, userLibraryListId));
   }
 
+  function favouriteAdd() {
+    // console.log("api item details", apiItemDetails);
+    // console.log("category id", categoryId);
+    // console.log("User library id", userLibraryListId);
+    dispatch(
+      addItemToList(favouriteApiItemDetails, categoryId, userFavouriteListId)
+    );
+  }
+
   const itemInLibrary = allListItems?.find((i: any) => {
     return i.list.type === "Library" && i.item.apiId === itemId;
   });
-  console.log("Item in library", itemInLibrary);
+
+  const itemInFavourites = allListItems?.find((i: any) => {
+    return i.list.type === "Favourites" && i.item.apiId === itemId;
+  });
+  console.log("Item in favourites", itemInFavourites);
 
   function onClickRemove() {
     dispatch(removeItemFromLibrary(itemId));
+  }
+
+  function favouriteRemove() {
+    dispatch(removeItemFromFavourites(itemId));
   }
 
   return (
@@ -86,7 +129,15 @@ export default function LibraryItemDetails() {
           Add to Library
         </Button>
       )}
-      <Button variant="outline-dark">Favourites</Button>
+      {itemInFavourites ? (
+        <Button onClick={favouriteRemove} variant="outline-dark">
+          Remove from Favourites
+        </Button>
+      ) : (
+        <Button onClick={favouriteAdd} variant="outline-dark">
+          Favourites
+        </Button>
+      )}
     </div>
   );
 }
