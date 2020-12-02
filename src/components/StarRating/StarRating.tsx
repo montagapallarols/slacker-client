3f2cf6dd8026bbe07b254fde78de98cd0acc0071
montipallarols <montaga.pallarols@gmail.com>
2020-12-02 20:31:59 +0100

    Set up rating value for each star

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./StarRating.css";
 import { FaStar } from "react-icons/fa";
+
 export default function StarRating() {
+  const [rating, setRating] = useState(null);
+  console.log("Rating", rating);
   return (
     <div>
-      {[...Array(5)].map((s: any) => {
-        return <FaStar size={40} />;
+      {[...Array(5)].map((s: any, i) => {
+        const ratingValue: any = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar size={40} className="star" />
+          </label>
+        );
       })}
     </div>
   );
