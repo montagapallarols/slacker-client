cd0c832a44b23b532063ed5a5b5a7d46e24b7a4e
montipallarols <montaga.pallarols@gmail.com>
2020-12-30 16:26:43 +0100

    Add filter buttons in My Profile to choose between lists/reviews

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -38,7 +38,8 @@ import {
   BsClockFill,
   BsClock,
 } from "react-icons/bs";
-import { Col, Image, Button, Form } from "react-bootstrap";
+import { Col, Image, Button, Form, Media } from "react-bootstrap";
+import { Divider, Grid } from "@material-ui/core";
 
 export default function MyProfile() {
   const dispatch = useDispatch();
@@ -55,6 +56,7 @@ export default function MyProfile() {
   const history = useHistory();
 
   const [editProfile, setEditProfile] = useState(false);
+  const [profileButton, setProfileButton] = useState("lists");
 
   useEffect(() => {
     if (!user.token) {
@@ -113,6 +115,7 @@ export default function MyProfile() {
     <div>
       <div className="background-image">
         <h1 className="profile-name">{`${userProfile?.firstName} ${userProfile?.lastName}`}</h1>
+
         <div className="profile-image">
           <img src={userProfile?.imageUrl} />
         </div>
@@ -169,139 +172,167 @@ export default function MyProfile() {
           </Form>
         ) : null}
       </div>
-
-      <div className="list">
-        <div className="list-card">
-          <h3>
-            <BsStarFill /> Favourites
-          </h3>
-          <div className="item-list">
-            {userFavourites?.map((f: any) => {
-              return (
-                <div key={f.itemId}>
-                  <Link
-                    to={`/my-profile/${user.id}/favourites/${f.item.apiId}`}
-                    className="link"
-                  >
-                    <div key={f.id} className="item-card">
-                      <p>{f.item.name}</p>
-                      {allCategories?.map((c: any) => {
-                        return c.id === f.item.categoryId ? (
-                          <em key={c.id}>
-                            <p className="category-name">({c.name})</p>
-                          </em>
-                        ) : null;
-                      })}
-                      {f.item.poster === "N/A" ? null : (
-                        <img src={f.item.poster} alt="poster" height="150px" />
-                      )}
+      <Button
+        onClick={() => setProfileButton("lists")}
+        style={{ margin: "10px" }}
+        variant="outline-dark"
+      >
+        Lists
+      </Button>
+      <Button
+        onClick={() => setProfileButton("reviews")}
+        variant="outline-dark"
+      >
+        Reviews
+      </Button>
+      {profileButton === "lists" ? (
+        <div>
+          <div className="list">
+            <div className="list-card">
+              <h3>
+                <BsStarFill /> Favourites
+              </h3>
+              <div className="item-list">
+                {userFavourites?.map((f: any) => {
+                  return (
+                    <div key={f.itemId}>
+                      <Link
+                        to={`/my-profile/${user.id}/favourites/${f.item.apiId}`}
+                        className="link"
+                      >
+                        <div key={f.id} className="item-card">
+                          <p>{f.item.name}</p>
+                          {allCategories?.map((c: any) => {
+                            return c.id === f.item.categoryId ? (
+                              <em key={c.id}>
+                                <p className="category-name">({c.name})</p>
+                              </em>
+                            ) : null;
+                          })}
+                          {f.item.poster === "N/A" ? null : (
+                            <img
+                              src={f.item.poster}
+                              alt="poster"
+                              height="150px"
+                            />
+                          )}
+                        </div>
+                        <Button
+                          onClick={handleClickRemove}
+                          value={f.item.apiId}
+                          variant="outline-dark"
+                        >
+                          Remove
+                        </Button>
+                      </Link>
                     </div>
-                    <Button
-                      onClick={handleClickRemove}
-                      value={f.item.apiId}
-                      variant="outline-dark"
-                    >
-                      Remove
-                    </Button>
-                  </Link>
-                </div>
-              );
-            })}
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="list">
+            <div className="list-card">
+              <h3>
+                <BsHeartFill /> Library
+              </h3>
+
+              <div className="item-list">
+                {allCategories?.map((c: any) => {
+                  return (
+                    <div key={c.id} className="item-card">
+                      <p>{c.name}</p>
+                      {c.name === "Films" ? (
+                        <img
+                          src="https://storage.googleapis.com/ff-storage-p01/festivals/logos/000/039/385/large/logo.jpg?1498668430"
+                          alt="logo"
+                          height="100px"
+                          className="film-image"
+                        />
+                      ) : (
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUHuZo6NHZMspV5r5Lv_JT3_sCp9PEkl774g&usqp=CAU"
+                          alt="logo"
+                          height="80px"
+                        />
+                      )}
+                      <Link to={`/my-profile/${user.id}/library/${c.name}`}>
+                        <Button variant="outline-dark">See list</Button>
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="list">
+            <div className="list-card">
+              <h3>
+                <BsClockFill /> Wishlist
+              </h3>
+              <div className="item-list">
+                {allCategories?.map((c: any) => {
+                  return (
+                    <div key={c.id} className="item-card">
+                      <p>{c.name}</p>
+                      {c.name === "Films" ? (
+                        <img
+                          src="https://storage.googleapis.com/ff-storage-p01/festivals/logos/000/039/385/large/logo.jpg?1498668430"
+                          alt="logo"
+                          height="100px"
+                          className="film-image"
+                        />
+                      ) : (
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUHuZo6NHZMspV5r5Lv_JT3_sCp9PEkl774g&usqp=CAU"
+                          alt="logo"
+                          height="80px"
+                        />
+                      )}
+                      <Link to={`/my-profile/${user.id}/wishlist/${c.name}`}>
+                        <Button variant="outline-dark">See list</Button>
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="list">
-        <div className="list-card">
-          <h3>
-            <BsHeartFill /> Library
-          </h3>
-
-          <div className="item-list">
-            {allCategories?.map((c: any) => {
-              return (
-                <div key={c.id} className="item-card">
-                  <p>{c.name}</p>
-                  {c.name === "Films" ? (
-                    <img
-                      src="https://storage.googleapis.com/ff-storage-p01/festivals/logos/000/039/385/large/logo.jpg?1498668430"
-                      alt="logo"
-                      height="100px"
-                      className="film-image"
-                    />
-                  ) : (
-                    <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUHuZo6NHZMspV5r5Lv_JT3_sCp9PEkl774g&usqp=CAU"
-                      alt="logo"
-                      height="80px"
-                    />
-                  )}
-                  <Link to={`/my-profile/${user.id}/library/${c.name}`}>
-                    <Button variant="outline-dark">See list</Button>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
+      ) : (
+        <div>
+          {profileReviews?.map((r: any) => {
+            return (
+              <div key={r.id}>
+                <h4 className="review-item-name">{r.item.name}</h4>
+                {r.item.poster === "N/A" ? null : (
+                  <img
+                    className="review-image"
+                    src={r.item.poster}
+                    alt="poster"
+                    height="130px"
+                  />
+                )}
+                <em>
+                  <h5>{r.name}</h5>
+                </em>
+                <em>
+                  <p className="plot">{r.content}</p>
+                </em>
+                <Rating name="read-only" value={r.rating} readOnly />
+                <em>
+                  <p>
+                    {r.profile.firstName} {r.profile.lastName}
+                  </p>
+                </em>
+                <hr style={{ width: "60%" }} />
+              </div>
+            );
+          })}
         </div>
-      </div>
-
-      <div className="list">
-        <div className="list-card">
-          <h3>
-            <BsClockFill /> Wishlist
-          </h3>
-          <div className="item-list">
-            {allCategories?.map((c: any) => {
-              return (
-                <div key={c.id} className="item-card">
-                  <p>{c.name}</p>
-                  {c.name === "Films" ? (
-                    <img
-                      src="https://storage.googleapis.com/ff-storage-p01/festivals/logos/000/039/385/large/logo.jpg?1498668430"
-                      alt="logo"
-                      height="100px"
-                      className="film-image"
-                    />
-                  ) : (
-                    <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUHuZo6NHZMspV5r5Lv_JT3_sCp9PEkl774g&usqp=CAU"
-                      alt="logo"
-                      height="80px"
-                    />
-                  )}
-                  <Link to={`/my-profile/${user.id}/wishlist/${c.name}`}>
-                    <Button variant="outline-dark">See list</Button>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-      <h3 className="reviews-title">My Reviews</h3>
-      {profileReviews?.map((r: any) => {
-        return (
-          <div key={r.id}>
-            <h4 className="review-item-name">{r.item.name}</h4>
-            {r.item.poster === "N/A" ? null : (
-              <img src={r.item.poster} alt="poster" height="100px" />
-            )}
-            <em>
-              <h5>{r.name}</h5>
-            </em>
-            <em>
-              <p className="plot">{r.content}</p>
-            </em>
-            <Rating name="read-only" value={r.rating} readOnly />
-            <em>
-              <p>
-                {r.profile.firstName} {r.profile.lastName}
-              </p>
-            </em>
-          </div>
-        );
-      })}
+      )}
     </div>
   );
 }
