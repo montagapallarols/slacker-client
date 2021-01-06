8a1c61ef4580d0943aa9db7c49ab3d3ef8008877
montipallarols <montaga.pallarols@gmail.com>
2021-01-06 23:02:53 +0100

    Ammend redux dev tools index file

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,19 +12,13 @@
 // );
 
 import { createStore, applyMiddleware, compose } from "redux";
-
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
-
+const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__
+  ? (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  : (x: string) => x;
+const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 const store = createStore(reducer, enhancer);
 
 export default store;
