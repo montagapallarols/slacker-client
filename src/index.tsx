6a84738690823141b945c16638d894eff2e80399
montipallarols <montaga.pallarols@gmail.com>
2021-01-06 23:06:43 +0100

    Ammend redux dev tools index file

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { store } from "./store";
+import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
