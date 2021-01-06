3274935de69378a4d4b37f16cc2aa65aa5b735e6
montipallarols <montaga.pallarols@gmail.com>
2021-01-06 22:54:49 +0100

    Ammend redux dev tools index file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,30 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import ReduxThunk from "redux-thunk";
-import rootReducer from "./rootReducer";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import ReduxThunk from "redux-thunk";
+// import rootReducer from "./rootReducer";
 
-export const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(ReduxThunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// export const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(ReduxThunk),
+//     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+//       (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+import { createStore, applyMiddleware, compose } from "redux";
+
+import ReduxThunk from "redux-thunk";
+import reducer from "./rootReducer";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
+
+const store = createStore(reducer, enhancer);
+
+export default store;
